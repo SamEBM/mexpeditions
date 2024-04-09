f645446e7db6778e62870f19598231202e31bdfb
Samuel E. Benitez Miranda <samuelebm@Samuels-MacBook-Air.local>
2024-04-08 22:42:35 -0600

    Adding responsiveness and last details

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  scroll() { 
+    window.scrollBy(0, window.innerHeight);
+  } 
 }
